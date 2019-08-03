6c8675b38cc6ca05394503f0ad353cb70e282176
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-03 15:16:29 -0500

    f

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var voting_time_limit = 5
 var max_question_id = 144
 
 //Connect to mongo
-MongoClient.connect("mongodb+srv://oof:Oooofers1!@quipr-test1-exc7k.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }, (err, cl) => {
+MongoClient.connect(MONGODB_URI, { useNewUrlParser: true }, (err, cl) => {
 	if(err){
 		throw err;
 	}

55b11e14fc74b453d9076bb7bf3089dbd38d50d6
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-03 18:36:53 -0500

    update

--- public/connectSocket.js
+++ public/connectSocket.js
@@ -8,6 +8,6 @@ var port = '5000'
 function returnSocket(connectionType){
 	// connectionType: 0=localhost 1=ngrok
 	
-	var socket_server_ip = ['http://localhost:' + port, 'https://cb99026a.ngrok.io']
+	var socket_server_ip = ['https://stark-scrubland-68406.herokuapp.com/', 'https://cb99026a.ngrok.io']
 	return io.connect(socket_server_ip[connectionType]);
 }

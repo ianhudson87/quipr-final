6814c5954840a4ce4892565d924ba86b95cd122b
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-03 18:05:01 -0500

    a

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
 const MongoClient = require('mongodb').MongoClient
 
 let port = process.env.PORT;

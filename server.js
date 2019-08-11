03796edfb73679ffdbdf83257888c01d83a9fd14
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-10 23:34:08 -0500

    bug fix

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ MongoClient.connect('mongodb+srv://oof:Oooofers1!@quipr-test1-exc7k.mongodb.net/
 			users = db.collection('users')
 			
 			// Show people's votes
-			user.find({'game_name': game_name, 'vote': 1}).toArray((err, res1) => {
+			users.find({'game_name': game_name, 'vote': 1}).toArray((err, res1) => {
 				// res1 = users in game who voted for player 1
 				users.find({'game_name': game_name, 'vote': 1}).toArray((err, res2) => {
 					// res2 = users in game who voted for player 2

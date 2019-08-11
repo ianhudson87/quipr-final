721bd4231e769a2e25508f8f1d91776066ec0efb
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-11 00:11:20 -0500

    Update connectSocket.js

--- public/connectSocket.js
+++ public/connectSocket.js
@@ -1,4 +1,4 @@
 function returnSocket(){
 	var socket_server_ip = ['http://quipr-final.herokuapp.com/', 'https://cb99026a.ngrok.io', 'http://localhost:4000']
-	return io.connect(socket_server_ip[2]);
+	return io.connect(socket_server_ip[0]);
 }

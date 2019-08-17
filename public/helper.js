a22901915a3baf5ae70551c9d6f3ff3750db6d7a
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2019-08-17 02:49:51 -0500

    Update helper.js

--- public/helper.js
+++ public/helper.js
@@ -1,6 +1,6 @@
 function returnSocket(){
 	var socket_server_ip = ['http://quipr-final.herokuapp.com/', 'https://cb99026a.ngrok.io', 'http://localhost:4000']
-	return io.connect(socket_server_ip[2]);
+	return io.connect(socket_server_ip[0]);
 }
 
 function getStageAndRefreshIfNeeded(stage, local_storage_game_name){
